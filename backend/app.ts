0acb850bb342a06148eadefca2abd52a1fbbb875
YOUNGJUNE <cerealmaster@naver.com>
2023-11-16 17:19:26 +0900

    initial backend commit

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,14 @@
 import express, {Application, Request, Response} from 'express';
 
+const dbkey = process.env.DBKEY;
 const app : Application = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 
 app.get('/', (req:Request, res:Response)=>{
     res.send('Hello world!');
 })
+
+
 
 app.listen(port, ()=>{
     console.log(`listening on ${port}`);
