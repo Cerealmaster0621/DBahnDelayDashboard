c792628f28e74c71407dfb93c147d9196314ca5d
YOUNGJUNE <cerealmaster@naver.com>
2023-11-16 17:10:39 +0900

    backend initial commit

---
+++ backend/app.ts
@@ -0,0 +1,12 @@
+import express, {Application, Request, Response} from 'express';
+
+const app : Application = express();
+const port = 3000;
+
+app.get('/', (req:Request, res:Response)=>{
+    res.send('Hello world!');
+})
+
+app.listen(port, ()=>{
+    console.log(`listening on ${port}`);
+})
